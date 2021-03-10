1ffd47bea0c7fd57ec84d22ba20eeb1aa1976280
rodrigodojo <rodrigodoj@gmail.com>
2021-03-10 18:41:47 -0300

    adicionando funçao no botão save

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -4,14 +4,21 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import gui.util.Constraints;
+import gui.util.Utils;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable{
 
+	private Department entity;
+	
+	private DepartmentService service;
+	
 	@FXML
 	private TextField txtId;
 	@FXML
@@ -23,11 +30,27 @@ public class DepartmentFormController implements Initializable{
 	@FXML
 	private Button btCancel;
 	
-	@FXML
-	public void onBtSaveAction() {
-		System.out.println("onBtSaveAction");
+	public void setDepartment(Department entity) {
+		this.entity = entity;
 	}
 	
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
+	}
+	
+	@FXML
+	public void onBtSaveAction() {
+		entity = getFormData();
+		service.saveOrUpdate(entity);
+	}
+	
+	private Department getFormData() {
+		Department obj = new Department();
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		obj.setName(txtName.getText());
+		return obj;
+	}
+
 	@FXML
 	public void onBtCancelAction() {
 		System.out.println("onBtCancelAction");
@@ -41,6 +64,15 @@ public class DepartmentFormController implements Initializable{
 	private void initializeNodes() {
 		Constraints.setTextFieldInteger(txtId);
 		Constraints.setTextFieldMaxLength(txtName, 30);
+	}
+	
+	public void updateFormData() {
+		if(entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+			txtId.setText(String.valueOf(entity.getId()));
+			txtName.setText(entity.getName());
+
 	}
 
 }
