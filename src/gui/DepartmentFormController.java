b29096f758377513ec0c17805347d7662e290c11
rodrigodojo <rodrigodoj@gmail.com>
2021-03-11 09:43:21 -0300

    atualizando a classe

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,10 +3,14 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -39,9 +43,20 @@ public class DepartmentFormController implements Initializable{
 	}
 	
 	@FXML
-	public void onBtSaveAction() {
+	public void onBtSaveAction(ActionEvent event) {
+		if(entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if(service == null) {
+			throw new IllegalAccessError("Service was null");
+		}
+		try {		
 		entity = getFormData();
 		service.saveOrUpdate(entity);
+		Utils.currentStage(event).close();
+		}catch(DbException e) {
+			Alerts.showAlert("Error saving object", null,e.getMessage() , AlertType.ERROR);
+		}
 	}
 	
 	private Department getFormData() {
@@ -52,8 +67,8 @@ public class DepartmentFormController implements Initializable{
 	}
 
 	@FXML
-	public void onBtCancelAction() {
-		System.out.println("onBtCancelAction");
+	public void onBtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
 	}
 	
 	@Override
